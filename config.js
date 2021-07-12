5f116f810effc2f57ea7f63fb8e409ee7c79bb22
Prasanth Sanjeevi <prasanths@sssbi.com>
2021-07-12 15:49:05 +0530

    config file key removed

--- config.js
+++ config.js
@@ -1,9 +1,3 @@
-const config = {
-  endpoint: "https://azurecosmoskapturas.documents.azure.com:443/",
-  key: "E6IHZTLqEf8eaIgqrweRDXRcQXHWinRIoCMaD7Gyqr854S5yxzXXgRu3Fo6M4gP94kKQgRPUHp9l7J5RQ5wuyg==",
-  databaseId: "Tasks",
-  containerId: "Items",
-  partitionKey: { kind: "Hash", paths: ["/category"] },
-};
+const config = {};
 
 module.exports = config;
